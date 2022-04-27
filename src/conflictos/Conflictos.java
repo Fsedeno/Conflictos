e68bf7e2063f85c9800899a255da65eca50692e6
Francisco Sedeño Barea <sedenobareaf@iesmartinrivero.org>
2022-04-27 14:41:48 +0200

    Creando Conflictos

--- src/conflictos/Conflictos.java
+++ src/conflictos/Conflictos.java
@@ -15,7 +15,8 @@ public class Conflictos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Francisco");
+        System.out.println("Aitor Manco");
+        
         ClaseFrancisco fco=new ClaseFrancisco();
     }
     
