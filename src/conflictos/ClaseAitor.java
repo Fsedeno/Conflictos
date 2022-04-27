b6b6149e6644c0a381c98bac50b4c6c131eb2d69
Aitor Garcia <garciaruanoaj@iesmartinrivero.org>
2022-04-27 14:28:18 +0200

    Añadida clase Aitor

---
+++ src/conflictos/ClaseAitor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conflictos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ClaseAitor {
+    String string = "Clase creada por Aitor :)";
+}
